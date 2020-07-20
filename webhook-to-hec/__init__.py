5787100cd4b5c951f7467d578d1d26155db36f9c
Ryan O'Connor <roconnor@splunk.com>
2020-07-20 11:22:36 -0400

    first commit

---
+++ webhook-to-hec/__init__.py
@@ -0,0 +1,32 @@
+import logging
+import urllib3
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    #Parse URL Parameters
+    request_args = req.params
+
+    http_method = request_args.get('http_method')
+    url = request_args.get("url")
+    port = request_args.get('port')
+    token = request_args.get('token')
+
+    #Build Full URL out of Parameters
+    full_url = http_method+'://'+url+':'+port+'/services/collector/raw'
+    
+    #Get Body
+    webhook_body = req.get_body()
+
+
+    #Create URLLib3 Pool Manager
+    http = urllib3.PoolManager()
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    
+    #Post Event
+    r = http.request('POST', full_url, body=webhook_body, headers={'Content-Type': 'application/json', 'Authorization':'Splunk '+token})
+    return func.HttpResponse(
+             "This was successfully posted to "+full_url,
+             status_code=200
+    )
